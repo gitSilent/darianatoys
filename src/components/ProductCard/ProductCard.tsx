507ec985df6446782123764a261ea82316be2266
Dmitry <dmitrym2019@mail.ru>
2023-07-16 18:49:33 +0300

    Сверстана страница products

--- src/components/ProductCard/ProductCard.tsx
+++ src/components/ProductCard/ProductCard.tsx
@@ -1,9 +1,24 @@
 import React from 'react'
+import toys from '../../media/toys.jpg'
+import to_cart from '../../media/to_cart.svg'
 
-export default function ProductCard() {
+interface IProps {
+  img: string,
+  desc:string,
+  price:string
+}
+
+export default function ProductCard({img, desc, price}:IProps) {
   return (
-    <div>
-        
+    <div className='min-w-[350px] max-w-[480px] min-h-[460px] max-h-[560px] relative border border-gray-400 rounded-xl hover:cursor-pointer hover:scale-[101%] duration-[150ms] '>
+        <img src={toys} alt="product" className='object-cover w-full min-h-[350px] rounded-tr-xl rounded-tl-xl ' />
+        <div className='px-5 pb-5 flex flex-row justify-between items-center'>
+          <div className=''>
+            <span className='block mt-4 font-semibold text-xl'>{desc}</span>
+            <span className='block font-normal text-2xl mt-3'>{price} р.</span>
+          </div>
+          <img src={to_cart} alt="add to cart" className='w-[72px] h-[72px] hover:cursor-pointer hover:scale-110 duration-500 ' />
+        </div>
     </div>
   )
 }
--- src/pages/Products/Products.tsx
+++ src/pages/Products/Products.tsx
@@ -1,7 +1,25 @@
 import React from 'react'
+import Header from '../../components/Header'
+import Footer from '../../components/Footer'
+import ProductCard from '../../components/ProductCard/ProductCard'
 
 export default function Products() {
   return (
-    <div>Products</div>
+    <div className='pt-[78px]'>
+      <Header/>
+      <h2 className='font-bold m-auto w-fit text-xl pt-16'>Товары</h2>
+      <div className='mt-11 px-3 flex flex-col items-center lg:flex-row lg:flex-wrap lg:gap-5 lg:justify-center'>
+        <ProductCard img="" desc="Плюшевый медведь" price="1500"/>
+        <ProductCard img="" desc="Плюшевый медведь" price="1500"/>
+        <ProductCard img="" desc="Плюшевый медведь" price="1500"/>
+        <ProductCard img="" desc="Плюшевый медведь" price="1500"/>
+        <ProductCard img="" desc="Плюшевый медведь" price="1500"/>
+        <ProductCard img="" desc="Плюшевый медведь" price="1500"/>
+        <ProductCard img="" desc="Плюшевый медведь" price="1500"/>
+        <ProductCard img="" desc="Плюшевый медведь" price="1500"/>
+        
+      </div>
+      <Footer/>
+    </div>
   )
 }
