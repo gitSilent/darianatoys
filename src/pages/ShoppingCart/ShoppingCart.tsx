281579d2afa7697a6829aacc52d74cdbca42e233
Dmitry <dmitrym2019@mail.ru>
2023-07-22 23:40:50 +0300

    написаны шаблоны для запросов feedback и добавление/просмотр корзины

--- src/pages/ShoppingCart/ShoppingCart.tsx
+++ src/pages/ShoppingCart/ShoppingCart.tsx
@@ -1,20 +1,36 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import CartItem from '../../components/CartItem/CartItem'
 import Header from '../../components/Header'
 import Footer from '../../components/Footer'
+import { getCart } from '../../services/api/cart'
+import { IToysInCart } from '../../types/types'
 
 export default function ShoppingCart() {
+
+  const [toysInCart, setToysInCart] = useState<IToysInCart>()
+
+  useEffect(() => {
+    
+    getCart()
+    .then((response)=>{
+      console.log(response);
+      setToysInCart(response.data)
+    })
+    
+  }, [])
+  
+
   return (
     <div className='pt-[105px] h-screen justify-between'>
       <Header/>
       <h2 className='font-bold m-auto w-fit text-xl lg:pt-16'>Корзина</h2>
 
       <div className='px-10'>
-        <CartItem/>
-        <CartItem/>
-        <CartItem/>
-        <CartItem/>
-        <span className='block mt-16 font-medium text-4xl w-fit m-auto'>Итого: 3 000 р.</span>
+        {toysInCart?.items.map((item,i)=>(
+          <CartItem img={""} name={"toy name"} desc={"toy desc"} amount={item.amount} priceForOne={1000} sumPrice={1000}/>
+        ))}
+      
+        <span className='block mt-16 font-medium text-4xl w-fit m-auto'>Итого: {toysInCart?.total_price} р.</span>
         <button className='block rounded-xl mt-10 py-3 px-10 font-bold text-2xl w-fit bg-amber-600 m-auto hover:bg-amber-700'>Оформить заказ</button>
       </div>
       <Footer/>
