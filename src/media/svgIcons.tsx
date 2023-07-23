617c857372b38eecb67baf3ee119a1867eee85c0
Hosur-r <skyebr@yandex.ru>
2023-07-23 15:42:10 +0300

    new header

--- src/media/svgIcons.tsx
+++ src/media/svgIcons.tsx
@@ -1,4 +1,3 @@
-import { JsxElement } from "typescript"
 
 export const LogoSvg: JSX.Element =
     <svg width="120" height="47" viewBox="0 0 176 47" fill="none" xmlns="http://www.w3.org/2000/svg" className="xs:w-[120px] sl:w-[176px]">
@@ -15,4 +14,9 @@ export const profileSvg: JSX.Element =
 export const cartSvg: JSX.Element =
     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="svgIcons cursor-pointer hover:text-amber-800 transition-all duration-150">
         <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 10.5V6a3.75 3.75 0 10-7.5 0v4.5m11.356-1.993l1.263 12c.07.665-.45 1.243-1.119 1.243H4.25a1.125 1.125 0 01-1.12-1.243l1.264-12A1.125 1.125 0 015.513 7.5h12.974c.576 0 1.059.435 1.119 1.007zM8.625 10.5a.375.375 0 11-.75 0 .375.375 0 01.75 0zm7.5 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z" />
+    </svg>
+
+export const burgerIcon: JSX.Element =
+    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={2} stroke="currentColor" className="w-11 h-11">
+        <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
     </svg>
