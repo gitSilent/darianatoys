39487ef0b2916ecc9e2463d504c7ea5a8163c5ba
Hosur-r <skyebr@yandex.ru>
2023-07-23 14:51:39 +0300

    product card && add to cart

--- src/components/ProductCard/ProductCard.tsx
+++ src/components/ProductCard/ProductCard.tsx
@@ -1,31 +1,35 @@
-import React from 'react'
-import toys from '../../media/toys.jpg'
 import to_cart from '../../media/to_cart.svg'
 import { Link } from 'react-router-dom'
 import { IProduct } from '../../types/types'
+import { toast } from 'react-toastify'
+import { addToCart } from '../../services/api/cart'
 
-// interface IProps {
-//   id:string,
-//   img: string,
-//   desc:string,
-//   price:string
-// }
+function handleCartBtn() {
+  addToCart({
+    toy: "1",
+    user: "2",
+    amount: 1
+  }).then((response) => {
+    toast(response?.data?.responce)
+  }).catch((er: any) => {
+    toast("Ошибка, товар не был добавлен в корзину")
+  })
+}
 
 export default function ProductCard({ slug, photos, description, cost }: IProduct) {
-
-
   return (
-    <Link to={`/product/?id=${slug}`}>
-      <div className='min-w-[350px] max-w-[480px] min-h-[460px] max-h-[560px] relative border border-gray-400 rounded-xl hover:cursor-pointer hover:scale-[101%] duration-[150ms] '>
+    <div className='min-w-[350px] max-w-[480px] min-h-[460px] max-h-[560px] relative border border-gray-400 rounded-xl hover:cursor-pointer hover:scale-[101%] duration-[150ms] '>
+      <Link to={`/product/?id=${slug}`}>
         <img src={photos[0].photo} alt="product" className='object-cover w-full min-h-[350px] rounded-tr-xl rounded-tl-xl ' />
-        <div className='px-5 pb-5 flex flex-row justify-between items-center'>
-          <div className=''>
-            <span className='block mt-4 font-semibold text-xl'>{description}</span>
-            <span className='block font-normal text-2xl mt-3'>{cost} р.</span>
-          </div>
-          <img src={to_cart} alt="add to cart" className='w-[72px] h-[72px] hover:cursor-pointer hover:scale-110 duration-500 ' />
+      </Link>
+      <div className='px-5 pb-5 flex flex-row justify-between items-center'>
+        <div className=''>
+          <span className='block mt-4 font-semibold text-xl'>{description}</span>
+          <span className='block font-normal text-2xl mt-3'>{cost} р.</span>
         </div>
+        <img src={to_cart} alt="add to cart" className='w-[72px] h-[72px] hover:cursor-pointer hover:scale-110 duration-500' onClick={handleCartBtn} />
       </div>
-    </Link>
+    </div>
+
   )
 }
